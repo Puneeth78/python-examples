0485c3f596424d835d8c7315cdcf2b25a598273a
Puneeth V <PuneethV78@gmail.com>
2025-11-30 08:44:30 +0530

    added file

--- main6.py
+++ main6.py
@@ -1,21 +1,21 @@
 
 # logic building program
 # sum of natural numbners
-# n=int(input("enter the number: "))
-# sum=0
-# while n>0:
-#     sum+=n
-#     n-=1
-# print(sum)
+n=int(input("enter the number: "))
+sum=0
+while n>0:
+    sum+=n
+    n-=1
+print(sum)
 
 # reverse number
-# num=int(input("enter the number: "))
-# rev=0
-# while num>0:
-#     digit=num%10
-#     rev=rev*10+digit
-#     num//=10
-# print(f"the reverse of {num} is :",rev)
+num=int(input("enter the number: "))
+rev=0
+while num>0:
+    digit=num%10
+    rev=rev*10+digit
+    num//=10
+print(f"the reverse of {num} is :",rev)
 
 # cheak the num is even or odd 
 number=int(input("enter the number: "))
